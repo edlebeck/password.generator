3a6a86c58d8418d84d13c92c5c2801ecaa12110c
Alan Edlebeck <alan.edlebeck@gmail.com>
2022-03-12 08:18:19 -0600

    added random number function

--- script.js
+++ script.js
@@ -4,14 +4,24 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// random number function
+function random (min, max) {
+  var num = Math.floor(Math.random() * (max - min + 1) + min);
+  return num;
+}
+// generate password function
+function generatePassword() {
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
+  //console.log(password);
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
